5ab3d595b728cf6c1cedfc97999bdb9cbbc41056
VickArmand <victormaina1962@gmail.com>
2022-10-20 22:04:07 -0700

    resolving

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -9,21 +9,19 @@ void print_to_98(int n)
 		for (; n <= 98; n++)
 		{
 			printf("%d", n);
-			_putchar(',');
-			_putchar(' ');
+			printf(", ");
 		}
 	}
 	else if (n == 98)
 	{
 		printf("%d", n);
-		_putchar(',');
-		_putchar(' ');
 	}
 	else
 	{
 		for (; n >= 98; n--)
 		{
 			printf("%d", n);
+			printf(", ");
 		}
 	}
 
