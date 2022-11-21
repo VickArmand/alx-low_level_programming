f2637bf07af2c2c872f153149158c08bb1244144
VickArmand <victormaina1962@gmail.com>
2022-11-21 13:31:27 -0800

    resolving

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,12 +1,12 @@
 #include "main.h"
 
 /**
- * print_numbers - a function that prints the numbers, from 0 to 9.
+ * print_most_numbers - a function that prints the numbers, from 0 to 9.
  * Do not print 2 and 4.
  * followed by a new line.
  * Return: Always 0
  */
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -6,7 +6,7 @@ int _isdigit(int c);
 
 int mul(int a, int b);
 
-void print_numbers(void);
+void print_number(void);
 
 void print_most_numbers(void);
 
