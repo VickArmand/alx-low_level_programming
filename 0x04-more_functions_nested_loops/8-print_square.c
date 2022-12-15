10421400ee93456efa227cd9fd750cabc65328d5
VickArmand <victormaina1962@gmail.com>
2022-12-15 06:37:05 -0800

    resolving

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -18,8 +18,11 @@ void print_square(int size)
 	{
 		for (i = 0; i < size; i++) 
 		{
-			_putchar('#');
+			for (j = 0; j < size; j++)
+			{			
+				_putchar('#');
+			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
 }
