4d1597d944f98c61d8218ab255728523e2515c6f
VickArmand <victormaina1962@gmail.com>
2023-01-16 18:22:32 -0800

    create lib_dynamic

--- 0x18-dynamic_libraries/5-strstr.c
+++ 0x18-dynamic_libraries/5-strstr.c
@@ -7,14 +7,12 @@
  * @needle: substring.
  * Return: pointer to the beginning of located substring or
  * NULL if the substring is not found.
- */
+*/
 
 char *_strstr(char *haystack, char *needle)
 {
 	char *bhaystack;
-
 	char *pneedle;
-
 
 	while (*haystack != '\0')
 	{
@@ -23,16 +21,12 @@ char *_strstr(char *haystack, char *needle)
 
 		while (*haystack != '\0' && *pneedle != '\0' && *haystack == *pneedle)
 		{
-
 			haystack++;
 			pneedle++;
 		}
-
 		if (!*pneedle)
 			return (bhaystack);
 		haystack = bhaystack + 1;
 	}
 	return (0);
-
 }
-
