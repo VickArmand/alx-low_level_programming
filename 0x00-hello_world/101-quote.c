29aa40a6e7fd4c6e8df049f246175ca054760052
VickArmand <victormaina1962@gmail.com>
2023-06-16 04:49:39 +0300

    quote

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
-
-#include <stdlib.h>
+#include <unistd.h>
 /**
  * main - Entry point*
  *
@@ -9,7 +8,7 @@
  */
 int main(void)
 {
-	write(STDOUT_FILENO, "and that piece of art is useful
-			\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful"
+			"\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
