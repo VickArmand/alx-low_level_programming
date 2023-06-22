a74871a19ca4289f0fe89d4fe48869b8cf4a1539
VickArmand <victormaina1962@gmail.com>
2023-06-22 20:02:19 +0300

    positive and largest

---
+++ 0x03-debugging/0-positive_or_negative.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * positive_or_negative - determines the positivity or
+ * negativity of a random number
+ * @i: number parameter
+ */
+void positive_or_negative(int i)
+{
+	if (i > 0)
+	{
+		printf("%d is positive\n", i);
+	}
+	else if (i < 0)
+	{
+		printf("%d is negative\n", i);
+	}
+	else
+	{
+		printf("%d is zero\n", i);
+	}
+}
