5db8287ea190a2c423fc420fa7c3e4d88392c1f1
VickArmand <victormaina1962@gmail.com>
2023-06-30 05:53:43 +0300

    reverse

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -2,6 +2,26 @@
 
 /**
  * reverse_array - reverses the content of an array of integers
- * @a: 
- * @n:
+ * @a: array of integers
+ * @n: number of elements of the array
  */
+
+void reverse_array(int *a, int n)
+{
+	int i = 0, temp;
+
+	while (i < n)
+	{
+		if (i < (n / 2))
+		{
+			temp = a[i];
+			a[i] = a[n - i - 1];
+			a[n - i - 1] = temp;
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+}
