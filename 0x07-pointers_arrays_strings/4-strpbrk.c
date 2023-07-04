103467b17265d346a4e7812bbf83ff2528ee8a74
VickArmand <victormaina1962@gmail.com>
2023-07-04 06:25:25 +0300

    strpbrk

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,22 @@
 #include "main.h"
 
 /**
+ * _strlen - returns the length of a string.
+ * @s: string to be used
+ * Return: length of string
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*(s + len) != '\0')
+		len++;
+
+	return (len);
+}
+
+/**
  * _strpbrk - locates the first occurrence in the string s
  * of any of the bytes in the string accept
  * @s: char pointer
@@ -11,18 +27,23 @@
 
 char *_strpbrk(char *s, char *accept)
 {
-	int i = 0, res;
+	int i = 0, j;
+	int len1 = _strlen(s), len2 = _strlen(accept);
+	int res = len1, ispresent = 0;
 
-	while (*(s + i) != '\0')
+	while (i < len2)
 	{
-		if (s[i] == accept[i] && s[i + 1] == accept[i + 1])
+		for (j = 0; j < len1; j++)
 		{
-			res = i;
-			break;
+			if (s[j] == accept[i])
+			{
+				ispresent = 1;
+				res = j <= res ? j : res + 0;
+			}
 		}
 		i++;
 	}
-	if (res)
+	if (ispresent == 1)
 		return (s + res);
 	else
 		return (NULL);
