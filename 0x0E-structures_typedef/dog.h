05248a734f607ce9c48fc8a73316af6f78fa036b
VickArmand <victormaina1962@gmail.com>
2023-07-18 03:59:35 +0300

    init dog

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -15,4 +15,9 @@ struct dog
 	float age;
 	char *owner;
 };
+typedef struct dog dog_t;
+void init_dog(struct dog *d, char *name, float age, char *owner);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+void print_dog(struct dog *d);
 #endif
