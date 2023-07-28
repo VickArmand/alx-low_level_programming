03b04561e032eca76480d42d6b17cf0099592f07
VickArmand <victormaina1962@gmail.com>
2023-07-28 21:14:35 +0300

    add node end correct

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,20 +13,19 @@ list_t *add_node_end(list_t **head, const char *str)
 	int i;
 
 	node_end = malloc(sizeof(list_t));
-	temp = *head;
 
 	if (node_end == NULL)
 	{
 		return (NULL);
 	}
-	if (head == NULL)
+	if (*head == NULL)
 	{
-		node_end->next = NULL;
 		*head = node_end;
 	}
 	else
 	{
-		while (temp != NULL)
+		temp = *head;
+		while (temp->next != NULL)
 			temp = temp->next;
 		temp->next = node_end;
 	}
@@ -35,15 +34,15 @@ list_t *add_node_end(list_t **head, const char *str)
 		i = 0;
 		while (str[i] != '\0')
 		{
-			node_end->str[i] = str[i];
 			i++;
 		}
-		node_end->str[i] = '\0';
-		node_end->len = i - 1;
+		node_end->str = strdup(str);
+		node_end->len = i;
 	}
 	else
 	{
 		node_end->len = 0;
 	}
+	node_end->next = NULL;
 	return (node_end);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -19,6 +19,7 @@ typedef struct list_s
 #include <stdio.h>
 #include <stddef.h>
 #include <stdlib.h>
+#include <string.h>
 int _putchar(char c);
 size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
