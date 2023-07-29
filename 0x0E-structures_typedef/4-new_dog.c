cbd0d791d4eba86d9c0db38b2f6ea853ca8c1ff2
VickArmand <victormaina1962@gmail.com>
2023-07-29 18:01:44 +0300

    new dog and copy of owner and name

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -2,6 +2,37 @@
 #include <stdlib.h>
 
 /**
+ * len - find length of string
+ * @str: string
+ * Return: length
+ */
+int len(char *str)
+{
+	int i;
+
+	for (i = 0; *(str + i); i++)
+		;
+	return (i);
+}
+
+/**
+ * _strcpy - copies the string pointed to by src,
+ * including the terminating null byte (\0),
+ * to the buffer pointed to by dest
+ * @dest: copy source to this buffer
+ * @src: this is the source to copy
+ * Return: copy of original source
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	for (i = 0; i <= len(src); i++)
+		dest[i] = src[i];
+	return (dest);
+}
+
+/**
  * new_dog - creates a new dog
  * @name: dog name
  * @age: dog age
@@ -11,15 +42,42 @@
 
 dog_t *new_dog(char *name, float age, char *owner)
 {
-	dog_t *nw_dog;
+	dog_t *dog1;
+	char *copy_of_name;
+	char *copy_of_owner;
 
-	nw_dog = malloc(sizeof(struct dog));
-	if (nw_dog)
+	dog1 = malloc(sizeof(dog_t));
+	if (dog1 == NULL)
+		return (NULL);
+
+	dog1->age = age;
+
+	if (name != NULL)
 	{
-		nw_dog->name = name;
-		nw_dog->age = age;
-		nw_dog->owner = owner;
-		return (nw_dog);
+		copy_of_name = malloc(len(name) + 1);
+		if (copy_of_name == NULL)
+		{
+			free(dog1);
+			return (NULL);
+		}
+		dog1->name = _strcpy(copy_of_name, name);
 	}
-	return (NULL);
+	else
+		dog1->name = NULL;
+
+	if (owner != NULL)
+	{
+		copy_of_owner = malloc(len(owner) + 1);
+		if (copy_of_owner == NULL)
+		{
+			free(copy_of_name);
+			free(dog1);
+			return (NULL);
+		}
+		dog1->owner = _strcpy(copy_of_owner, owner);
+	}
+	else
+		dog1->owner = NULL;
+
+	return (dog1);
 }
--- 0x0E-structures_typedef/tests/4-main.c
+++ 0x0E-structures_typedef/tests/4-main.c
@@ -9,7 +9,8 @@
 int main(void)
 {
     dog_t *my_dog;
-
+new_dog("Ghost", 4.75, "Jon Snow");
+new_dog("Ghost", 4.75, "Jon Snow");
     my_dog = new_dog("Poppy", 3.5, "Bob");
     printf("My name is %s, and I am %.1f :) - Woof!\n", my_dog->name, my_dog->age);
     return (0);
