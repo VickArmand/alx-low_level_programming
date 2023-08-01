734f98977978802b04031e06a182f7c333e417e7
VickArmand <victormaina1962@gmail.com>
2023-08-01 09:56:39 +0300

    print list int

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - prints all the elements of a listint_t list
+ * @h: pointer to the head of linked list
+ * Return: size of linked list
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		h = h -> next;
+		size++;
+	}
+	return (size);
+}
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -18,7 +18,7 @@ typedef struct listint_s
 #include <stdlib.h>
 #include <string.h>
 int _putchar(char c);
-size_t print_list(const list_t *h);
+size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
 listint_t *add_nodeint(listint_t **head, const int n);
 listint_t *add_nodeint_end(listint_t **head, const int n);
