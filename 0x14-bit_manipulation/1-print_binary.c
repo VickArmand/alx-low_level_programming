d5b1a4e18b20fd645908b46fd9dc91b8dd4df081
VickArmand <victormaina1962@gmail.com>
2023-08-08 17:19:33 +0300

    1-print_binary.c

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,6 @@
 #include "main.h"
 #include <stdio.h>
 
-void print_number(unsigned long int num, unsigned long int divisor);
-
 /**
  * print_binary - prints the binary representation of a number
  * @n: the decimal number
@@ -14,41 +12,12 @@ void print_number(unsigned long int num, unsigned long int divisor);
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int rem, res, i;
-
-	if (n == 0)
-		_putchar('0');
+	if (n != 0)
+	{
+		if (n >> 1)
+			print_binary(n >> 1);
+		_putchar((n & 1) + '0');
+	}
 	else
-	{
-		res = 0;
-		i = 1;
-		while (n != 0)
-		{
-			rem = n & 1;
-			res += i * rem;
-			n = n >> 1;
-			i *= 10;
-		}
-		print_number(res, (i / 10));
-	}
-}
-
-/**
- * print_number - prints the whole number
- * @num: number
- * @divisor: divisor
- * Return: void
- */
-
-void print_number(unsigned long int num, unsigned long int divisor)
-{
-	int n;
-
-	while (divisor > 0)
-	{
-		n = num / divisor;
-		_putchar(n + '0');
-		num %= divisor;
-		divisor /= 10;
-	}
+		_putchar('0');
 }
