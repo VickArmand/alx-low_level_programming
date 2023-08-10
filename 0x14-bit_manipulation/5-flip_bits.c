c508a47c3e621193e789048b8df9a1592a91bec5
VickArmand <victormaina1962@gmail.com>
2023-08-10 15:25:04 +0300

    4-clear_bit.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * flip_bits - returns the number of bits you would need to
+ * flip to get from one number to another
+ * @n: decimal number 
+ * @m: decmal number
+ * Return: the number of bits you would need to flip
+ * to get from one number to another
+ * You are not allowed to use the % or / operators
+ */
+
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int num_bits;
+
+
+}
