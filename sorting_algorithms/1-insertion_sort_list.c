9f916d25a3f2f59476bf0c95182920ccc7dc0418
VickArmand <victormaina1962@gmail.com>
2023-10-09 16:35:00 +0300

    refactor

--- sorting_algorithms/1-insertion_sort_list.c
+++
@@ -1,5 +0,0 @@
-#include "sort.h"
-void insertion_sort_list(listint_t **list)
-{
-    
-}
--- sorting_algorithms/swap.c
+++
@@ -1,7 +0,0 @@
-#include "sort.h"
-void swap(int *array,size_t index1, size_t index2)
-{
-    int temp = array[index1];
-    array[index1] = array[index2];
-    array[index2] = temp;
-}
