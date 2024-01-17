1c4c13707871e787bf774b9176ed865382a6b9f5
VickArmand <victormaina1962@gmail.com>
2024-01-17 21:22:48 +0300

    first commit

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,16 @@
+#include "search_algos.h"
+/**
+ * linear_search - searches for a value in a sorted array of integers
+ * using the linear search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: Your function must return the first index where value is located
+ * If value is not present in array or if array is NULL,
+ * your function must return -1
+ * Every time you compare a value in the array to the value
+ * you are searching, you have to print this value
+ */
+int linear_search(int *array, size_t size, int value)
+{
+}
---
+++ 0x1E-search_algorithms/1-binary.c
@@ -0,0 +1,18 @@
+#include "search_algos.h"
+/**
+ * binary_search - searches for a value in a sorted array of integers
+ * using the Binary search algorithm
+ * @array: is a pointer to the first element of the array to search in
+ * @size: is the number of elements in array
+ * @value: is the value to search for
+ * Return: Your function must return the index where value is located
+ * You can assume that array will be sorted in ascending order
+ * You can assume that value won’t appear more than once in array
+ * If value is not present in array or if array is NULL,
+ * your function must return -1
+ * You must print the array being searched every time it changes.
+ * (e.g. at the beginning and when you search a subarray)
+ */
+int binary_search(int *array, size_t size, int value)
+{
+}
