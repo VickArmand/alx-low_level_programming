29504bc6754c5328600234a7a750b1ee36fb37af
VickArmand <victormaina1962@gmail.com>
2024-01-18 12:58:20 +0300

    0-linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -13,4 +13,15 @@
  */
 int linear_search(int *array, size_t size, int value)
 {
+	size_t i = 0;
+
+	if (!array)
+		return (-1);
+	for (; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
+	return (-1);
 }
